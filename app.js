f2b94970d014ef640be2d6a0729ddfd95dd97f27
= <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2021-08-07 19:44:26 +0100

    config for both dotenv and config

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express('express');
+
+//for dotenv
+const dotenv = require('dotenv')
+dotenv.config({ path : './config/config.env'  })
+
+// for config
+const config = require('config')
