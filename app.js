e4e469ead5c9ca83461fee6b999f6c1e4dcd28ba
= <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2021-08-10 19:14:44 +0100

    little changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express('express');
+dotenv.config({ path: './config/config.env' });
+const app = express();
 
-//for dotenv
-const dotenv = require('dotenv')
-dotenv.config({ path : './config/config.env'  })
+const PORT = process.env.PORT || 3600;
 
-// for config
-const config = require('config')
+app.listen(PORT, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+});
